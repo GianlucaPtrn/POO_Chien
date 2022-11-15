b31b6e57405fe7189f47db08431f986246e281ac
GianlucaPtrn <200188@site.asty-moulin.be>
2022-11-15 14:15:32 +0100

    Add Projet chien

---
+++ POO_Intro_13Sept/POO_Intro_13Sept/Chien.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Intro_13Sept
+{
+    class Chien
+    {
+        //attribut privé par covention commence par un "_" 
+        private string _nom;
+        private string _race;
+        private int _age;
+
+        //méthode publiques
+
+        public String Nom
+        {
+            get { 
+                return _nom; 
+            }
+            set {
+                _nom = value; 
+            }
+        }
+
+        public String Race
+        {
+            get
+            {
+                return _race;
+            }
+            set
+            {
+                _race = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+
+        //le construteur
+        public Chien(string nom, string race, int age)
+        {
+            this._nom = nom;
+            this._race = race;
+            this._age = age;
+        }
+
+        public string afficheCaracteristique()
+        {
+            string phrase = "Nom : " + _nom + "|" + " Age : " + _age + "|" + " Race : " + _race;
+            return phrase;
+        }
+    }
+}
---
+++ POO_Intro_13Sept/POO_Intro_13Sept/Program.cs
@@ -0,0 +1,53 @@
+﻿using POO_Intro_13Sept;
+using System;
+
+namespace POO_Intro 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenu dans notre refuge");
+            Console.WriteLine();
+
+            Chien[] mesChiens = new Chien[3];
+            mesChiens[0] = new Chien("Rox", "Canniche", 4);
+            mesChiens[1] = new Chien("Rox", "Canniche", 4);
+            mesChiens[2] = new Chien("Rox", "Canniche", 4);
+            float moyenneChien = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Quel est le nom de votre chien ?");
+                mesChiens[i].Nom = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Quel est la race de votre chien ?");
+                mesChiens[i].Race = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Quel est l'age de votre chien ?");
+                mesChiens[i].Age = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                mesChiens[i] = new Chien(mesChiens[i].Nom, mesChiens[i].Race, mesChiens[i].Age);
+                Console.WriteLine(mesChiens[i].afficheCaracteristique());
+
+                moyenneChien = moyenneChien + mesChiens[i].Age;
+            }
+            Console.WriteLine();
+            Console.WriteLine("la moyenne d'age est de " + moyenneChien/3);
+
+            mesChiens[0].Age += 10;
+            mesChiens[1].Age += 10;
+            mesChiens[2].Age += 10;
+
+            Console.WriteLine();
+            Console.WriteLine(mesChiens[1].Age);    
+
+        }
+    }
+}
